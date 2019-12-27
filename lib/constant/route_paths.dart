27b8ad3142277ef489e25160c5cc4734d0f54212
amjadalfawal <37481122+amjadalfawal@users.noreply.github.com>
2019-12-27 19:50:37 +0200

    repo structure build

---
+++ lib/constant/route_paths.dart
@@ -0,0 +1,10 @@
+const String LoginRoute = 'Login';
+const String HomeRoute = 'Home';
+const String SingUp = 'SingUp';
+const String Profile = 'Profile';
+const String Setting = 'Setting';
+const String IntialRoute = '/';
+const String ProfileRoute = 'Prfoile';
+const String UniList = 'UniList';
+const String FrindeList = 'FrindeList';
+const String ChatMessge = 'ChatMessge';
